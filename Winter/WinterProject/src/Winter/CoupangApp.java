d01f48945d4b00d682141fc59f6945990fa38bc1
JinsuPark01 <156520699+JinsuPark01@users.noreply.github.com>
2024-02-04 00:08:11 +0900

    Update CoupangApp.java

--- Winter/WinterProject/src/Winter/CoupangApp.java
+++ Winter/WinterProject/src/Winter/CoupangApp.java
@@ -1,6 +1,5 @@
 package Winter.WinterProject.src.Winter;
 
-
 public class CoupangApp {
 
 	public static void main(String[] args) {
@@ -16,15 +15,6 @@ public class CoupangApp {
 		coupang.addProduct(new Member(3333, "훈이"), new Product("친구", 1500, 1));
 		coupang.addProduct(new Member(3333, "훈이"), new Product("짱돌", 2500, 1));
 
-
-
-		//실행시 1.로그인 2.회원가입
-		//접속시 1.상품등록 2.상품삭제 3.개인정보확인 4. 구매 등등 ==> Coupang에 작성할것 로그인 성공시 메소드로!
 		coupang.start();
-
-
 	}
-
-
-
 }
